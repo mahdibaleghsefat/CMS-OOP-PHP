a3965698067a996130e03f3ad871b10c8f2cac70
mahdibaleghsefat <m.baleghsefat@gmail.com>
2019-05-20 19:18:07 +0430

    The connection to the database has been completed.

---
+++ includes/navigation.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="">
+				<span class="sr-only"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="" class="navbar-brand">strt</a>
+		</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collaps-1">
+			<ul class="nav navbar-nav">
+				<li>
+					<a href="">about</a>
+				</li>
+				<li>
+					<a href="">services</a>
+				</li>
+				<li>
+					<a href="">admin</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include("includes/header.php"); ?>
+
+<div class="row">
+	<div class="col-md-8">
+		<?php include("includes/navigation.php"); ?>
+	</div>
+
+	<div class="col-md-4">
+		<?php include("includes/sidebar.php"); ?>
+	</div>
+</div>
+
+<?php include("includes/footer.php"); ?>
