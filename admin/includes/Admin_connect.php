73644e9e293e9cb1bb98bd813feb517cd4a3780f
mahdibaleghsefat <m.baleghsefat@gmail.com>
2019-05-22 16:00:22 +0430

    added methods to class : User (Methods : update, delete) . edit page : Admin_connect

--- admin/includes/Admin_connect.php
+++ admin/includes/Admin_connect.php
@@ -4,10 +4,18 @@
 
 $user = new User();
 
-$user->username = "sdg";
-$user->password = "sdg";
-$user->first_name = "sdg";
-$user->last_name = "sdg";
-$user->create();
+// $user->username = "sdg";
+// $user->password = "sdg";
+// $user->first_name = "sdg";
+// $user->last_name = "sdg";
+// $user->create();
+
+// $user = User::find_user_by_id(2);
+// $user->last_name = "lsdfjn lsjf";
+// $user->update();
+
+$user = User::find_user_by_id(4);
+$user->delete();
+
 
 ?>
